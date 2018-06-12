4683642b3084c5f8ad442945e8b69fb519106201
Miriam <miriamrolvering@web.de>
2018-06-12 16:45:05 +0200

    - Little improvements

--- karteikarten_app_mwb/src/Components/EditorComponent.js
+++ karteikarten_app_mwb/src/Components/EditorComponent.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import './EditorComponent.css';
 
 export class EditorComponent extends Component {
 
     constructor(props) {
-        super(props)
-        this.state = { text: '' } // You can also pass a Quill Delta here
-        this.handleChange = this.handleChange.bind(this)
+        super(props);
+        this.state = { editorHtml: '' }; // You can also pass a Quill Delta here
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(value) {
-        this.setState({ text: value })
+        this.setState({ editorHtml: value })
     }
+
+    modules = {
+        toolbar: [
+            [{ 'header': [1, 2, 3, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            ['link', 'image'],
+            ['clean']
+        ]
+    };
 
     render() {
         return (
-            <ReactQuill theme="snow" value={this.state.text}
-                onChange={this.handleChange} />
-        )
+            <div className="text-editor">
+                <ReactQuill theme="snow"
+                    value={this.state.editorHtml}
+                    onChange={this.handleChange}
+                    modules={this.modules}
+                />
+            </div>
+        );
     }
 }
 
