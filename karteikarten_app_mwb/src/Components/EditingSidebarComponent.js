8b84bc166009ee6542c6409305d54f45a00cf3fa
Limpieza38 <enneking.britta@web.de>
2018-06-13 08:57:05 +0200

    - Folder Structur with toggle

--- karteikarten_app_mwb/src/Components/EditingSidebarComponent.js
+++ karteikarten_app_mwb/src/Components/EditingSidebarComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {MaterialIcon} from "./Helper/MaterialIcon";
+import {FolderComponent} from "./Helper/FolderComponent";
 
 export class EditingSidebarComponent extends Component {
     render() {
@@ -8,38 +9,7 @@ export class EditingSidebarComponent extends Component {
                 <div className="sidebar-sticky py-3">
                     <ul className="nav flex-column pl-1">
                         <li className="nav-item">
-                            <button className="d-flex align-items-center p-0">
-                                <MaterialIcon icon={'expand_more'}/>
-                                <MaterialIcon icon={"folder"}/>
-                                <span>Ordner Ebene 1</span>
-                            </button>
-
-                            <ul className="nav flex-column pl-md-4">
-
-                                <li>Karteikarte 1</li>
-                                <li>Karteikarte 2</li>
-                                <li>Karteikarte 3</li>
-                                <li>
-                                    <button className="d-flex align-items-center p-0">
-                                        <MaterialIcon icon={'expand_more'}/>
-                                        <MaterialIcon icon={"folder"}/>
-                                        <span>Ordner Ebene 2</span>
-                                    </button>
-                                    <ul className="nav flex-column pl-md-4">
-
-                                        <li>Karteikarte 5</li>
-                                        <li>Karteikarte 9</li>
-                                        <li>Karteikarte 312</li>
-                                        <li className="nav-item">
-                                            <button className="d-flex align-items-center p-0">
-                                                <MaterialIcon icon={'chevron_right'}/>
-                                                <MaterialIcon icon={"folder"}/>
-                                                <span>Ordner Ebene 3</span>
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
+                            <FolderComponent/>
 
                         </li>
                         <li className="nav-item">
