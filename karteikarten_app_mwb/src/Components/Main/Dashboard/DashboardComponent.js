9479c189b16ed649889e52edd95315f4148be248
Stefan Guelland <stefan@sguelland.de>
2018-07-11 09:07:06 +0200

    add path regex

--- karteikarten_app_mwb/src/Components/Main/Dashboard/DashboardComponent.js
+++ karteikarten_app_mwb/src/Components/Main/Dashboard/DashboardComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {DashboardContentComponent} from "./DashboardContentComponent";
 import {DashboardSidebarComponent} from "./DashboardSidebarComponent";
 import base from "../../../base";
+import PropTypes from "prop-types";
+
 
 export class DashboardComponent extends Component {
 
@@ -9,6 +11,7 @@ export class DashboardComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {folders: {}, currentfolder: undefined};
+        this.onClickFolder = this.onClickFolder.bind(this);
     }
 
     componentDidMount() {
@@ -25,11 +28,53 @@ export class DashboardComponent extends Component {
         base.removeBinding(this.folderRef);
     }
 
+
+    static contextTypes = {
+        router: PropTypes.shape({
+            history: PropTypes.shape({
+                push: PropTypes.func.isRequired,
+                replace: PropTypes.func.isRequired
+            }).isRequired,
+            staticContext: PropTypes.object
+        }).isRequired
+    };
+
+    onClickFolder(path) {
+        console.log(path);
+        const pathString = path.reduce((accumulator, currentValue) => (
+            accumulator + '/' + currentValue
+        ),'/overview');
+        console.log(pathString);
+
+        this.context.router.history.push(pathString);
+    }
+
     render() {
+        const path = this.context.router.route.match.params.path;
+        const data = [
+            {title: "myBook", id: 1, data: "myData"},
+            {title: "myBook2", id: 2, data: "myData2"},
+            {title: "myBook3", id: 3, data: "myData3"},
+            {title: "myBook4", id: 4, data: "myData4"},
+            {title: "myBook5", id: 5, data: "myData5"},
+            {title: "myBook6", id: 6, data: "myData6"},
+            // {title: "myBook7", id: 7, data: "myData7"},
+            {title: "myBook8", id: 8, data: "myData8"}
+        ]
         return (
             <div className="row">
-                <DashboardSidebarComponent changeCurrentfolder={this.changeCurrentfolder} folders={this.state.folders} addFolder={this.addFolder}/>
-                <DashboardContentComponent currentfolder={this.state.currentfolder} uid={this.props.uid}/>
+                <DashboardSidebarComponent
+                    changeCurrentfolder={this.changeCurrentfolder}
+                    folders={this.state.folders}
+                    addFolder={this.addFolder}
+                    onClickFolder={this.onClickFolder}
+                />
+                <DashboardContentComponent
+                    currentfolder={this.state.currentfolder}
+                    uid={this.props.uid}
+                    data={data}
+                    path={path}
+                />
             </div>
         )
     };
