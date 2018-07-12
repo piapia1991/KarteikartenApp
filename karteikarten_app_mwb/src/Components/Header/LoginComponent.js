210de1336c60c7f8315e4e216a271213b7ee4c59
Pia Piekarek <piapiekarek@googlemail.com>
2018-07-12 12:56:48 +0200

    layout changes to header and cards

--- karteikarten_app_mwb/src/Components/Header/LoginComponent.js
+++ karteikarten_app_mwb/src/Components/Header/LoginComponent.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import {Avatar, Typography, Grid, Button, IconButton, Icon, Tooltip, Hidden} from '@material-ui/core';
+import {Avatar, Typography, Grid, Button, IconButton, Icon, Tooltip} from '@material-ui/core';
 import {Link} from 'react-router-dom';
 
 export class LoginComponent extends Component {
@@ -12,22 +12,18 @@ export class LoginComponent extends Component {
             return (
                 <Fragment>
                     <Grid container>
-                        <Hidden only={['xs', 'sm', 'md']}>
-                            <Grid item xs={7}/>
-                        </Hidden>
-                        <Grid item xs={3} lg={8}>
+                        <Grid item xs={10} className="paddingTop-15">
                             <Grid container>
-                                <Grid item sm={8}>
-                                    <Typography variant="subheading" gutterBottom noWrap
-                                                className="paddingTop-10">{this.props.login.name}</Typography>
+                                <Grid item sm={10} className="paddingTop-5">
+                                    <Typography variant="subheading" gutterBottom noWrap>{this.props.login.name}</Typography>
                                 </Grid>
-                                <Grid item sm={4}>
-                                    <Avatar className="marginTop-3" alt="this.props.login.name" id="loginPicture"
+                                <Grid item sm={2}>
+                                    <Avatar alt="this.props.login.name" id="loginPicture"
                                             src={this.props.login.picture}/>
                                 </Grid>
                             </Grid>
                         </Grid>
-                        <Grid item xs={1} lg={2}>
+                        <Grid item xs={1} className="paddingTop-10">
                             <Tooltip id="tooltip-fab" title="Logout">
                                 <IconButton onClick={() => this.props.logout()}>
                                     <Icon className="exit_to_app">
@@ -36,7 +32,7 @@ export class LoginComponent extends Component {
                                 </IconButton>
                             </Tooltip>
                         </Grid>
-                        <Grid item xs={1} lg={2}>
+                        <Grid item xs={1} className="paddingTop-10">
                             <Tooltip id="tooltip-fab" title="Einstellungen">
                                 <Link to={"/settings"}>
                                     <IconButton to={"/settings"}>
--- karteikarten_app_mwb/src/Components/Main/Dashboard/IndexCardComponent.js
+++ karteikarten_app_mwb/src/Components/Main/Dashboard/IndexCardComponent.js
@@ -3,15 +3,16 @@ import { withRouter } from 'react-router-dom';
 import { Card, CardMedia, CardContent, Typography, Button, CardActions, Icon, Tooltip, Grid } from '@material-ui/core';
 import PropTypes from "prop-types";
 import {withStyles} from "@material-ui/core/styles";
+import karteikarte from "../../../images/Karteikarte.svg";
 
 const styles = () =>({
     card: {
-        maxWidth: 345,
+        minHeight: 120,
+        backgroundImage: "url("+karteikarte +")",
+        backgroundPosition: "0px 3px",
+        backgroundSize: "100%",
     },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-    },
+
 });
 
 class IndexCardComponent extends Component {
@@ -19,30 +20,23 @@ class IndexCardComponent extends Component {
     render() {
         const {classes} = this.props;
         const WithRouter = withRouter(({ history }) => (
-            <Grid item sm={12} md={6} lg={4} className="marginRight-10">
+            <Grid item sm={12} md={6} lg={4}>
                 <Card className={classes.card}>
-                    <CardMedia className={classes.media}
-                        image="https://www.buero-bedarf-thueringen.de/1336110-thickbox_default/karteikarten-a6-liniert-ws-100st.jpg"
-                        title="test" />
                     <CardContent>
-                        <Typography gutterBottom variant="body2">{this.props.title}</Typography>
+                        <Grid container alignItems={'stretch'}>
+                            <Grid item xs>
+                                <Typography gutterBottom variant="body2">{this.props.title}</Typography>
+                            </Grid>
+                        </Grid>
                     </CardContent>
 
                     <CardActions>
                         <Grid container spacing={16}>
-                            <Grid item xs={8} />
+                            <Grid item xs={10} />
                             <Grid item xs={2}>
                                 <Tooltip id="tooltip-fab" title="Bearbeiten">
                                     <Button variant="fab" mini color="secondary" aria-label="add" onClick={() => history.push(`/editing/${this.props.cardId}`)} >
                                         <Icon>edit_icon</Icon>
-                                    </Button>
-                                </Tooltip>
-                            </Grid>
-                            <Grid item xs={2}>
-
-                                <Tooltip id="tooltip-fab" title="Teilen">
-                                    <Button variant="fab" mini color="primary" aria-label="share">
-                                        <Icon>share_icon</Icon>
                                     </Button>
                                 </Tooltip>
                             </Grid>
